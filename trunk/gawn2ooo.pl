3393ae30a1e0c4f548bd64712a76f89fd264b397
kps <kps@8199466c-0bbf-89c9-3f3d-767715a87399>
2007-02-12 20:41:30 +0000

    bug fixes

--- trunk/gawn2ooo.pl
+++ trunk/gawn2ooo.pl
@@ -259,7 +259,7 @@ sub cross_ref_designation
 	}
 	if ($graphviz) {
 		# alternatively, not starting with ~,%,- or others that are always NULL
-		if ($x =~ m/^[;@#=*>$&]/ and $x ne ';u') {
+		if ($x =~ m/^[\;\@\#\=\*\>\$\&]/ and $x ne ';u') {
 			$x = '@';  # => not NULL
 		}
 		else {
@@ -509,12 +509,10 @@ elsif ($graphviz) {
 	foreach my $f (keys %answer) {
 		(my $id, my $label) = $f =~ /^([^:]+):(.+)$/;
 		$id =~ tr/nvsa/0123/;
-		$label =~ tr/nvsa/0123/;
 		$labels{$id} = $label;
 		foreach my $link (@{$answer{$f}}) {
 			(my $lid, my $llabel) = $link =~ /^([^:]+):(.+)$/;
 			$lid =~ tr/nvsa/0123/;
-			$llabel =~ tr/nvsa/0123/;
 			$labels{$lid} = $llabel;
 			print OUTPUTFILE "    $id -- $lid;\n";
 		}
