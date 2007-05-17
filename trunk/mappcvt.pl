0a20fa4c45ce76e7fa57b8d1ee641131a04c39b2
kps <kps@8199466c-0bbf-89c9-3f3d-767715a87399>
2007-05-17 03:56:34 +0000

    initial version of wn version-updater

---
+++ trunk/mappcvt.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my %sensekeysold;
+my %sensekeysnew; # hash of lists; keys are offsets, list entries are sensekeys
+my %mappingover; # hash of lists; keys are offsets, entries are poss new offsets
+
+sub read_mapp_file {
+	(my $fn, my $pos) = @_;
+
+	open(MAPP, "<", $fn) or die "Could not open $fn: $!";
+	while (<MAPP>) {
+		chomp;
+		(my $key, my $rest) = /^([0-9]+) (.+)$/;
+		while ($rest =~ m/([0-9]+ [^ ]+) /g) {
+			push @{$mappingover{"$pos:$key"}}, "$pos:$1";
+			push @{$mappingover{"5:$key"}}, "5:$1" if ($pos==3);
+		}
+	}
+	close MAPP;
+}
+
+
+open(INDEX, "<", "/usr/share/wordnet2.1/index.sense") or die "Could not open index.sense for old: $!";
+
+while (<INDEX>) {
+	chomp;
+	(my $sk, my $pos, my $offset) = /^([^%]+%(.)[^ ]+) ([0-9]+)/;
+	$sensekeysold{$sk} = "$pos:$offset";
+}
+close INDEX;
+
+open(INDEX2, "<", "/usr/share/wordnet/index.sense") or die "Could not open index.sense for new: $!";
+
+while (<INDEX2>) {
+	chomp;
+	(my $sk, my $pos, my $offset) = /^([^%]+%(.)[^ ]+) ([0-9]+)/;
+	push @{$sensekeysnew{"$pos:$offset"}}, $sk;
+}
+close INDEX2;
+
+my $path='/home/kps/seal/mapps/mapping-21-30';
+read_mapp_file("$path/wn21-30.adj", 3);
+read_mapp_file("$path/wn21-30.adv", 4);
+read_mapp_file("$path/wn21-30.noun", 1);
+read_mapp_file("$path/wn21-30.verb", 2);
+
+open(ENWNPO, "<", "en2wn.po") or die "Could not open PO file: $!";
+
+while (<ENWNPO>) {
+	chomp;
+	if (/^msgstr/) {
+		s/^msgstr "//;
+		s/"$//;
+		my $msgstr = $_;
+		unless ($msgstr eq 'NULL' or $msgstr eq '' or $msgstr =~ /^Content-Type/) {
+			my $oldoff = $sensekeysold{$msgstr};  # in form "$pos:$offset";
+			if (exists($mappingover{$oldoff})) {
+				(my $w) = /^([^%]+)%/;
+				print "$msgstr -> ";
+				foreach my $newoff (@{$mappingover{$oldoff}}) {
+					# newoff looks like "1:00092663 0.118"
+					(my $trueoff, my $prob) = $newoff =~ /^([^ ]+) ([^ ]+)/;
+					foreach my $newsk (@{$sensekeysnew{$trueoff}}) {
+						(my $thisw) = $newsk =~ /^([^%]+)%/;
+						if ($w eq $thisw) {
+							print "$newsk $prob ";
+						} #possibly no matches => change to ""...
+					}
+				}
+				print "\n";
+			}
+			else {
+				print "$msgstr -> NOMAP\n";
+				# probably maps to NULL in new version!!
+			}
+		}
+	}
+}
+close ENWNPO;
+exit 0;
