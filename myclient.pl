7cd478a60d343b96f1135e30339611bc8d2cb653
Kevin Scannell <kscanne@users.noreply.github.com>
2007-12-18 15:34:03 +0000

    debugging, comments

--- myclient.pl
+++ myclient.pl
@@ -11,9 +11,11 @@ use Encode qw(encode);
 binmode STDOUT, ":bytes";
 my $server_url = 'http://borel.slu.edu:8080/RPC2';
 my $server = Frontier::Client->new(url => $server_url,  debug => 1, );
+# +00 is a hub (green), +11 is a word (red)
 # my $query = 'comhlíonadh+11';
 # my $query = 'dragan+11';
-my $query = 'zzz+11';
+# my $query = 'zzz+11';
+my $query = 'simplí+00';
 eval { $server->call('getSubGraph', $server->string(encode("utf8",$query)), $server->int(1)); };
-eval { print $server->call('isNodePresent', $server->string('garbage+11'))->value; };
+# eval { print $server->call('isNodePresent', $server->string('garbage+11'))->value; };
 exit 0;
