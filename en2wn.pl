fab701942d65b460aa165c191db6015c10627f9d
Kevin Scannell <kscanne@users.noreply.github.com>
2010-05-01 23:55:12 +0000

    To UTF-8

--- en2wn.pl
+++ en2wn.pl
@@ -3,12 +3,15 @@
 use strict;
 use warnings;
 
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
 # converts En-Ir dictionary in diolaim/c to a pot file that
 # is used for mapping the English defs to WN sense_keys
 
-open(ENIR, "<", "/home/kps/gaeilge/diolaim/c/en") or die "Could not open Eng-Ir dictionary: $!\n";
+open(ENIR, "<:utf8", "/home/kps/gaeilge/diolaim/c/en") or die "Could not open Eng-Ir dictionary: $!\n";
 
-print "msgid \"\"\nmsgstr \"\"\n\"Content-Type: text/plain; charset=ISO-8859-1\\n\"\n\n";
+print "msgid \"\"\nmsgstr \"\"\n\"Content-Type: text/plain; charset=UTF-8\\n\"\n\n";
 while (<ENIR>) {
 	chomp;
 	s/\[/{/g;
