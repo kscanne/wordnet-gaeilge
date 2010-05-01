944a4321978bb90a8eb871627a1193bbc81badc9
Kevin Scannell <kscanne@users.noreply.github.com>
2010-05-01 23:56:27 +0000

    works with UTF-8 PO file now

--- mapper-ui.pl
+++ mapper-ui.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use Locale::PO;
+use Encode 'decode';
 
 die "Usage: $0 [-p|-n]\n" unless ($#ARGV == 0 and $ARGV[0] =~ /^-[pn]/);
 my $parens = ($ARGV[0] =~ /^-p/);
@@ -40,6 +41,7 @@ my %gloss;
 sub get_glosses
 {
 	(my $fn, my $pos) = @_;
+	# data.adj, data.adv, etc. are ASCII only
 	open(TEMP, "<", $fn) or die "Could not open $fn: $!\n";
 	while (<TEMP>) {
 		chomp;
@@ -79,6 +81,7 @@ sub my_sort {
 	}
 }
 
+# ASCII only
 open(SENSEINDEX, "<", "index.sense") or die "Could not open index.sense: $!\n";
 while (<SENSEINDEX>) {
 	chomp;
@@ -100,22 +103,25 @@ my $aref;
 local $SIG{__WARN__} = 'my_warn';
 $aref = Locale::PO->load_file_asarray('en2wn.po');
 }
-open(OUTPUTPO, ">", "en2wn-new.po") or die "Could not output PO file: $!\n";
+open(OUTPUTPO, ">:utf8", "en2wn-new.po") or die "Could not output PO file: $!\n";
 foreach my $msg (@$aref) {
-	my $id = $msg->msgid();
+	# comm's are all Irish, only about 10 msgid have accents (cliché, etc.)
+	# and msgstr's are guaranteed pure ASCII - these are also the only
+	# ones that are changed below so no need to encode before doing so
+	my $id = decode('UTF-8',$msg->msgid());
 	my $str = $msg->msgstr();
-	my $comm = $msg->automatic();
+	$str =~ s/^"//;
+	$str =~ s/"$//;
+	my $comm = decode('UTF-8',$msg->automatic());
 	if ($scanning_p) {
 		$scanning_p = 0 if ($id =~ /^"$startmatch/);
 	}
 	if (defined($id) && defined($str) && defined($comm) && !$done_p && !$scanning_p) {
-		if ($str and $id and (!$parens or $id =~ /\(/)) {
-			if ($str eq '""' or (!$parens and $str eq '"NULL"')) {
+		if (!$parens or $id =~ /\(/) {
+			if ($str eq '' or (!$parens and $str eq 'NULL')) {
 				my $sid = $id;
 				$sid =~ s/^"//;
 				$sid =~ s/"$//;
-				$comm =~ s/^"//;
-				$comm =~ s/"$//;
 				$sid =~ s/ \([^)]+\)$//;   # strip disambig string in parens
 				(my $lemma, my $pos) = $sid =~ /^(.*)  (a|n|v|adv)$/;
 				$lemma =~ s/ /_/g;
@@ -127,7 +133,7 @@ foreach my $msg (@$aref) {
 #                   Not that much harder to just select the "1" for these
 						my $key = $cands[0];
 						$key =~ s/\|.*//;
-						$msg->msgstr($key);
+						$str = $key;
 					}
 					elsif ($parens) {
 						print "\n\nmsgid = $id needs a mapping to WN\n";
@@ -145,30 +151,35 @@ foreach my $msg (@$aref) {
 							$done_p = 1;
 						}
 						elsif ($ans =~ /^[Nn]/) {
-							$msg->msgstr('NULL');
+							$str = 'NULL';
 						}
 						elsif ($ans =~ /^[Ss]/) {
 							print "Leaving it unmapped.\n";
-							$msg->msgstr('');
+							$str = '';
 						}
 						elsif ($ans =~ /^[1-9][0-9]*$/) {
 							my $keeper = $cands[$ans-1];
 							$keeper =~ s/\|.+//;
-							$msg->msgstr($keeper);
+							$str = $keeper;
 						}
 						else {
 							print "Illegal input. Leaving it unmapped.\n";
-							$msg->msgstr('');
+							$str = '';
 						}
 					}
 				}
 				else {  # not in WN at all
-					$msg->msgstr("NULL");
+					$str = 'NULL';
 				}
 			}
 		}
 	}
-	print OUTPUTPO $msg->dump;
+	if (defined($comm) and $comm ne '') {
+		$comm =~ s/\n/\n#. /sg;
+		print OUTPUTPO "#. $comm\n" unless ($comm eq '');
+	}
+	print OUTPUTPO "msgid $id\n";
+	print OUTPUTPO "msgstr \"$str\"\n\n";
 }
 close OUTPUTPO;
 
